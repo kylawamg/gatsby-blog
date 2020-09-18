96f15652869eb2427b2c44f2802863d1c5869d62
alexseik <alexseik@hotmail.com>
2020-09-18 12:41:22 +0200

    feat: Update logo component with tailwind theme

--- src/components/logo.js
+++ src/components/logo.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React from "react"
 
 const Logo = () => {
   return (
     <div className="flex items-center flex-shrink-0 text-white mr-6">
       <a
         href="#"
-        className="text-secondary font-semibold text-xl tracking-tight self-center"
+        className="text-title_purple-primary font-semibold text-xl tracking-tight self-center"
       >
         &lt;<span className="text-primary font-bold text-xl">/</span>
-            devSpain&gt;
-          </a>
+        devSpain&gt;
+      </a>
     </div>
-  );
-};
+  )
+}
 
-export default Logo;
+export default Logo
