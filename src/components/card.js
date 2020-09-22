5c1787c89388221b5baf87d583271193cf55f8d6
Alejandro <alejandro.m.gomez.92@gmail.com>
2020-09-22 11:40:33 +0200

    styling card

--- src/components/card.js
+++ src/components/card.js
@@ -1,14 +1,20 @@
 import React from "react"
 import { Link } from "gatsby"
-
+import logo from "../assets/images/shape.png"
+import { HiOutlineArrowNarrowRight } from "react-icons/hi"
 const Card = ({ post }) => {
   return (
     <article className="mt-10 lg:mt-4 w-full h-card rounded-lg border border-title_purple-secondary bg-background_card shadow-card">
       <header className="custom-bg h-40 rounded-t-lg p-4">
-        <h2 className="text-2xl font-primary text-title_gray-secondary font-bold inline-block">
-          Ventajas de Deno sobre Nodejs
-        </h2>
-        <div className="mt-6">
+        <div className="flex">
+          <div className="pr-4 pt-1">
+            <img className="" src={logo}></img>
+          </div>
+          <h2 className="text-2xl font-primary text-title_gray-secondary font-bold">
+            Ventajas de Deno sobre Nodejs
+          </h2>
+        </div>
+        <div className="mt-10 pl-10">
           <time
             className="text-title_gray-secondary text-base inline font-primary"
             pubdate="true"
@@ -30,13 +36,26 @@ const Card = ({ post }) => {
         </div>
       </header>
 
-      <div className="p-4 font-primary text-title_purple-primary text-base">
+      <div className="p-4 font-secondary text-title_purple-primary text-base">
         <p>
           Lorem Ipsum is simply dummy text of the printing and typesetting
           industry. Lorem Ipsum has been the industry's standard.
         </p>
       </div>
-      <footer className="card__footer"></footer>
+      <footer className="w-full font-secondary text-title_purple-primary text-base">
+        <Link
+          to={`/blog`}
+          href="#responsive-header"
+          className="p-4 hover:underline active:underline float-right "
+        >
+          <HiOutlineArrowNarrowRight
+            style={{ fill: "#42316C" }}
+            size={26}
+            className="inline"
+          ></HiOutlineArrowNarrowRight>
+          <span className="font-semibold">Leer más</span>
+        </Link>
+      </footer>
     </article>
   )
 }
