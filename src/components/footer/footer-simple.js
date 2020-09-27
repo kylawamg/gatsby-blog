de2b007fd935e1290651ea6fce83fd39768fd6aa
alexseik <alexseik@hotmail.com>
2020-09-27 11:16:57 +0200

    fix: footer styles in md instead lg

--- src/components/footer/footer-simple.js
+++ src/components/footer/footer-simple.js
@@ -6,37 +6,33 @@ import { navigateWithScroll } from "../utils";
 
 const FooterSimple = () => {
   return (
-    <footer className="static bottom-0 custom-bg h-20">
+    <footer className="static bottom-0 custom-bg">
       <section className="container mx-auto w-full  lg:px-32 px-6">
-        <nav className="flex justify-between flex-wrap py-6 ">
+        <nav className="flex justify-between items-center flex-wrap py-6 ">
           <Logo />
-
-          <div className="w-full block flex-grow lg:flex lg:flex-row-reverse lg:items-center lg:self-end lg:w-auto">
-            <div className="text-title_gray-primary text-base divide-x">
-              <p className="block mt-4 lg:inline-block lg:mt-0  pr-2">
-                @devSpain
-              </p>
+          <div className="mb-1 font-secondary w-full block flex-grow md:flex md:flex-row-reverse md:items-center md:self-end md:w-auto">
+            <div className="text-title_gray-primary text-base md:divide-x">
               <Link
                 to={`/blog`}
                 href="#responsive-header"
-                className="hover:underline active:underline block mt-4 lg:inline-block lg:mt-0 pl-2 pr-2"
+                className="text-center mt-4 md:mt-0 hover:underline active:underline block md:inline-block lg:mt-0 pl-2 pr-2"
               >
                 Blog
               </Link>
               <a href={`/#training-section`}
                 onClick={navigateWithScroll.bind(this, 'training-section')}
-                className="hover:underline active:underline block mt-4 lg:inline-block lg:mt-0 pl-2 pr-2"
+                className="text-center mt-4 md:mt-0 hover:underline active:underline block md:inline-block lg:mt-0 pl-2 pr-2"
                 ariaLabel={`Scroll to training section`}>
                 {`Cursos`}
               </a>
               <a href={`/#about`}
                 onClick={navigateWithScroll.bind(this, 'about')}
-                className="hover:underline active:underline block mt-4 lg:inline-block lg:mt-0 pl-2 pr-2"
+                className="text-center mt-4 md:mt-0 hover:underline active:underline block md:inline-block lg:mt-0 pl-2 pr-2"
                 ariaLabel={`Scroll to about section`}>
                 {`Quiénes somos`}
               </a>
               <a href="mailto:admin@devspain.es"
-                className="hover:underline active:underline block mt-4 lg:inline-block lg:mt-0 pl-2 pr-2">
+                className="text-center mt-4 md:mt-0 hover:underline active:underline block md:inline-block lg:mt-0 pl-2 pr-2">
                 {`Contacto`}
               </a>
             </div>
--- src/components/logo.js
+++ src/components/logo.js
@@ -6,6 +6,7 @@ const Logo = () => {
     <div className="flex items-center flex-shrink-0 text-white mr-6">
       <Link
         to={`/`}
+        style={{fontFamily: "Inconsolata"}}
         href="#responsive-header"
         className="text-title_gray-primary font-semibold text-xl tracking-tight self-center"
       >
