0a55fe42ca5f1f1ae7c597ed8510388e5d4b1e64
alexseik <alexseik@hotmail.com>
2020-09-27 09:19:41 +0000

    Deploying to gh-pages from master de2b007fd935e1290651ea6fce83fd39768fd6aa

---
+++ component---src-pages-about-js-e2860376dc7167f54b50.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[7],{"3XHS":function(e,n,t){"use strict";t.r(n);var a=t("q1tI"),u=t.n(a);n.default=function(){return u.a.createElement("div",null,u.a.createElement("h1",null,"About page"))}}}]);
+//# sourceMappingURL=component---src-pages-about-js-e2860376dc7167f54b50.js.map
